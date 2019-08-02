299e02e99a8ccb41a7a4867ff9904ebfaeef7026
NataliaLira <natalia.rlira@gmail.com>
2019-08-02 15:38:17 -0300

    changing onclick element style

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JScalculator
 
 JScalculator challenge (not using any framework or library).
+
+You can check my calculator on this link: https://natalialira.github.io/JScalculator/
--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let buttons = document.querySelectorAll(".buttons");
+let ceButton = document.querySelector('#ce');
 let screen = document.querySelector(".screen-text");
 
 for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onmouseover = function (){
+        console.log("funfou");
+        this.style.border = "solid black 2px";
+        if (this == buttons[3]){
+            this.style.background = "#831a1a";
+        }else{
+        this.style.background = "#6d7279";
+        }
+        this.onmouseout = function (){
+            console.log("aeeeee rapaziada");
+            this.style.border = "solid #172837 2px";
+            if (this == buttons[3]){
+                this.style.background = "#a32222";
+            }else{
+            this.style.background = "#7a8289";
+            }
+        }
+
+    }
     buttons[i].onclick = function (){
         console.log("funfou");
         screen.append(this.innerText);    
     }
 }
+
+
