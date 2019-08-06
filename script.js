d43242b28b8d82313044a7f3439d20c07be5edf3
NataliaLira <natalia.rlira@gmail.com>
2019-08-06 10:51:10 -0300

    calculator working

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let buttons = document.querySelectorAll(".buttons");
 let ceButton = document.querySelector('#ce');
 let screen = document.querySelector(".screen-text");
 
+//------------stylizing buttons
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onmouseover = function (){
-        console.log("mouse_over");
+        //---debugging console.log("mouse_over");
         this.style.border = "solid black 2px";
         if (this == buttons[3]){
             this.style.background = "#831a1a";
@@ -12,7 +13,7 @@ for (let i = 0; i < buttons.length; i++) {
         this.style.background = "#6d7279";
         }
         this.onmouseout = function (){
-            console.log("mouse_out");
+            //---debugging console.log("mouse_out");
             this.style.border = "solid #172837 2px";
             if (this == buttons[3]){
                 this.style.background = "#a32222";
@@ -20,12 +21,20 @@ for (let i = 0; i < buttons.length; i++) {
             this.style.background = "#7a8289";
             }
         }
-
-    }
-    buttons[i].onclick = function (){
-        console.log("click");
-        screen.value = this.textContent;    
     }
 }
 
-
+//----calculating
+if (buttons[i].textContent == "="){
+    buttons[i].onclick = function (){
+    screen.value = eval(screen.value);
+    }
+} else if (buttons[i].textContent == "CE"){
+    buttons[i].onclick = function (){
+    screen.value = "";
+    } 
+} else {
+    buttons[i].onclick = function (){
+    screen.value += this.textContent;    
+    }
+}
